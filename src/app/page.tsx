6708e83e1dd81a58f783d7cbefbba1ad257931fd
Tushar3232 <anikdebnath916@gmail.com>
2026-02-24 16:31:52 +0600

    Display user data from session and handleLogout user Logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,32 @@
+'use client'
+import { signOut, useSession } from "next-auth/react"
+import Image from "next/image";
 
 export default function Home() {
+  const { data } = useSession();
+  console.log(data)
+
+  const handleLogout = async ()=>{
+    try{
+      await signOut()
+    }catch(error){
+
+    }
+  }
   return (
-    <div>
-        hello hioo
+    <div className=" min-h-screen flex flex-col items-center justify-center p-4 bg-black ">
+      <div className=" border border-white  text-white flex flex-col justify-center items-center p-8 gap-4">
+        <div className=" relative w-[100px] h-[100px] rounded-full border border-white overflow-hidden">
+          <Image src={data?.user?.image || "/practice-project2/public/next.svg"} alt="user Image" fill ></Image>
+
+        </div>
+        <h1 className=" text-2xl"> {data?.user?.name} </h1>
+        <p> {data?.user?.email} </p>
+
+        <button onClick={handleLogout} className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+          Logout 
+        </button>
+      </div>
     </div>
   )
 }
